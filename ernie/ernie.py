b081d97dbd70e987731a18826230a65b15059e68
Jonathan Stoppani <garetjax@Jonathan-Stoppanis-MacBook-Pro.local>
2010-08-02 21:12:33 +0200

    Refactored the whole class to feel more pythonic.

--- ernie/ernie.py
+++ ernie/ernie.py
@@ -1,4 +1,3 @@
-import os
 import logging
 import bert
 import struct
@@ -11,9 +10,7 @@ class Ernie(SocketServer.StreamRequestHandler):
     
     @classmethod
     def mod(cls, name):
-        if cls.mods.has_key(name) == False:
-            cls.mods[name] = Mod(name)
-        return cls.mods[name]
+        return cls.mods.setdefault(name, Mod(name))
     
     @classmethod
     def logfile(cls, file):
@@ -26,78 +23,74 @@ class Ernie(SocketServer.StreamRequestHandler):
             cls.logger.debug(text)
     
     def dispatch(self, mod, fun, args):
-        if Ernie.mods.has_key(mod) == False:
+        try:
+            mod = Ernie.mods[mod]
+        except KeyError:
             raise ServerError("No such module '" + mod + "'")
-        if Ernie.mods[mod].funs.has_key(fun) == False:
+        
+        try:
+            func = mod.funs[fun]
+        except KeyError:
             raise ServerError("No such function '" + mod + ":" + fun + "'")
-        return Ernie.mods[mod].funs[fun](*args)
+        
+        return func(*args)
     
-    def read_4(self, input):
-        raw = input.read(4)
-        if len(raw) == 0:
-            return None
-        return struct.unpack('!L', raw)[0]
+    def read_size(self):
+        raw = self.rfile.read(4)
+        return struct.unpack('!L', raw)[0] if raw else None
     
-    def read_berp(self, input):
-        packet_size = self.read_4(input)
-        if packet_size == None:
-            return None
-        ber = input.read(packet_size)
-        return bert.decode(ber)
+    def read_berp(self):
+        packet_size = self.read_size()
+        return bert.decode(self.rfile.read(packet_size)) if packet_size else None
     
-    def write_berp(self, output, obj):
+    def write_berp(self, obj):
         data = bert.encode(obj)
-        output.write(struct.pack("!L", len(data)))
-        output.write(data)
-        output.flush()
+        self.wfile.write(struct.pack("!L", len(data)))
+        self.wfile.write(data)
+        self.wfile.flush()
     
     def handle(self):
-        input = self.rfile
-        output = self.wfile
+        ipy = self.read_berp()
         
-        ipy = self.read_berp(input)
-        if ipy == None:
+        if not ipy:
             print 'Could not read BERP length header. Ernie server may have gone away. Exiting now.'
             exit()
         
-        if len(ipy) == 4 and ipy[0] == bert.Atom('call'):
+        self.log("-> " + ipy.__str__())
+        
+        if len(ipy) is 4 and ipy[0] == bert.Atom('call'):
             mod, fun, args = ipy[1:4]
-            self.log("-> " + ipy.__str__())
+            
             try:
                 res = self.dispatch(mod, fun, args)
-                opy = (bert.Atom('reply'), res)
-                self.log("<- " + opy.__str__())
-                self.write_berp(output, opy)
             except ServerError, e:
                 opy = (bert.Atom('error'), (bert.Atom('server'), 0, str(type(e)), str(e), ''))
-                self.log("<- " + opy.__str__())
-                self.write_berp(output, opy)
             except Exception, e:
                 opy = (bert.Atom('error'), (bert.Atom('user'), 0, str(type(e)), str(e), ''))
+            else:
+                opy = (bert.Atom('reply'), res)
+            finally:
                 self.log("<- " + opy.__str__())
-                self.write_berp(output, opy)
-        elif len(ipy) == 4 and ipy[0] == bert.Atom('cast'):
+                self.write_berp(opy)
+                
+        elif len(ipy) is 4 and ipy[0] == bert.Atom('cast'):
             mod, fun, args = ipy[1:4]
-            self.log("-> " + ipy.__str__())
             try:
                 res = self.dispatch(mod, fun, args)
-            except:
-                pass
-            self.write_berp(output, (bert.Atom('noreply')))
+            finally:
+                self.write_berp(bert.Atom('noreply'))
         else:
-            self.log("-> " + ipy.__str__())
             opy = (bert.Atom('error'), (bert.Atom('server'), 0, "Invalid request: " + ipy.__str__()))
             self.log("<- " + opy.__str__())
-            self.write_berp(output, opy)
-        
+            self.write_berp(opy)
+    
 
 class ServerError(Exception):
-    def __init__(self, value):
-        self.value = value
     def __str__(self):
-        return repr(self.value)        
+        return repr(self.args[0])
 
-class Mod:
+
+class Mod(object):
     def __init__(self, name):
         self.name = name
         self.funs = {}
